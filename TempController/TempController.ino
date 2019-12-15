a748d1bab09d8364117d7ae5bcb3772644717f08
Iza2 <Iza2@Iza2-HP.fios-router.home>
2019-12-15 10:54:35 -0500

    Local Sync

---
+++ TempController/TempController.ino
@@ -0,0 +1,275 @@
+
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//LCD includes
+#include <Wire.h>
+//#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+
+// #define DEBUG 1
+
+//LCD defines
+#define I2C_ADDR    0x27  // Define I2C Address where the PCF8574A is
+                          // Address can be changed by soldering A0, A1, or A2
+                          // Default is 0x27
+
+// map the pin configuration of LCD backpack for the LiquidCristal class
+#define BACKLIGHT_PIN 3
+#define En_pin  2
+#define Rw_pin  1
+#define Rs_pin  0
+#define D4_pin  4
+#define D5_pin  5
+#define D6_pin  6
+#define D7_pin  7
+
+float tempC;  // Current Temprature
+
+int RelayHeat = 6;
+int RelayCool = 7;
+
+const int RELAY_NONE = 0;
+const int RELAY_HEAT = 1;
+const int RELAY_COOL = 2;
+int RelayStatus = RELAY_NONE;
+
+
+const int SCREENMODE_DISPLAYTEMP = 1;
+const int SCREENMODE_SETUP = 2;
+const int SCREENMODE_SCREEN_SAVER = 3;
+int screenMode = SCREENMODE_DISPLAYTEMP;
+
+const int DISPLAYMODE_MAIN    = 0;
+const int DISPLAYMODE_TARGET  = 1;
+const int DISPLAYMODE_MINMAX  = 2;
+int currentDisplayMode = DISPLAYMODE_MAIN;
+
+unsigned long screenTime;
+
+const int UNIT_FARENHEIGHT = 1;
+const int UNIT_CELSIUS = 2;
+int unit =  UNIT_FARENHEIGHT;
+
+float deviation = 1.0;
+int targetTemp = 22;
+unsigned long tempratureTimer;
+
+LiquidCrystal_I2C lcd(I2C_ADDR,
+                      En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin,
+                      BACKLIGHT_PIN, POSITIVE);
+
+//temp sensor defines
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 9
+#define TEMPERATURE_PRECISION 9 // Lower resolution
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+int numberOfDevices; // Number of temperature devices found
+
+float min = 1024;
+float max = -100;
+
+DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
+
+void setup(void)
+{
+  displayLogo();
+  
+  //setup temp sensor
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo.");
+
+  // Start up the library
+  sensors.begin();
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i))
+    {
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    
+      Serial.println("Setting resolution to ");
+      Serial.println();
+      Serial.println(TEMPERATURE_PRECISION, DEC);
+      Serial.println();
+      // set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
+      sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+      delay(500);
+      Serial.print("Resolution actually set to: ");
+      Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+      delay(500);
+      Serial.println();
+      //break;
+    }else{
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }    
+  }
+
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print( "Setting up Rotary Encoder" );
+  //rotary encoder setup
+
+  rotarySetup();
+  Serial.begin (9600);
+
+  //
+  //  Releay Switch Setup
+  //
+  pinMode(RelayHeat, OUTPUT); //Set Pin12 as output 
+  pinMode(RelayCool, OUTPUT); //Set Pin12 as output 
+  digitalWrite(RelayHeat, HIGH); //Turn off relay 
+  digitalWrite(RelayCool, HIGH); //Turn off relay 
+  RelayStatus = RELAY_NONE;
+
+  bluetoothSetup();
+  
+  screenTime = millis();
+  tempratureTimer = millis();
+
+  delay( 4000 );
+  lcd.clear();
+  setTempratureTemplate();  
+}
+
+
+void loop(void)
+{ 
+  if(screenMode != SCREENMODE_SETUP ){
+    temperatureInterface();
+    relayControl();
+    bluetoothInterface();
+  }
+  rotary();
+}
+
+
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+    if(screenMode == SCREENMODE_DISPLAYTEMP ){
+      lcd.setCursor (5,0);         // go to col 16 of the last row
+      lcd.print( getDisplayTemperature( tempC ) );           // update the display with a new number
+  
+      //
+      //  Screensaver code
+      //
+      unsigned long delayTime = 60000 * 15;
+      unsigned long currentTime = millis();
+      if( screenTime + delayTime < currentTime ){
+        screenMode = SCREENMODE_SCREEN_SAVER;
+        lcd.setBacklight(LOW);
+      }
+    }
+}
+
+String getDisplayTemperature( float tempC ){
+    String strTemp = "";
+    if(unit == UNIT_CELSIUS)
+    {
+      strTemp = String(tempC);
+    }
+    else{
+      float tempF = DallasTemperature::toFahrenheit(tempC);
+      strTemp = String(tempF);
+    }
+    strTemp = strTemp.substring( 0, 4 );
+    return strTemp;
+}
+
+float getDisplayTemperatureNum( float tempC ){
+    float nTemp = tempC;
+    if(unit == UNIT_FARENHEIGHT)
+    {
+      nTemp = DallasTemperature::toFahrenheit(tempC);
+    }
+    return nTemp;
+}
+
+//
+//  Get Temprature Evry 1 Sec and Display
+//
+void temperatureInterface()
+{
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+
+  unsigned long currentTime = millis();
+  if( tempratureTimer + 1000 < currentTime ){
+
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    // Loop through each device, print out temperature data
+    for(int i=0;i<numberOfDevices; i++)
+    {
+      // Search the wire for address
+      if(sensors.getAddress(tempDeviceAddress, i))
+      {
+        // Output the device ID
+        //    Serial.print("Temperature for device: ");
+        //    Serial.println(i,DEC);
+        tempC = sensors.getTempC(tempDeviceAddress);
+        if(tempC >= max)
+        {
+          max = tempC;
+          if( currentDisplayMode == DISPLAYMODE_MINMAX ){
+            lcd.setCursor ( 11, 1 );            // go to the 2nd row
+            lcd.print( getDisplayTemperature( max ) ); // pad string with spaces for centering
+          }
+        }
+        if(tempC <= min)
+        {
+          min = tempC;
+          if( currentDisplayMode == DISPLAYMODE_MINMAX ){
+            lcd.setCursor ( 3, 1 );            // go to the 2nd row
+            lcd.print( getDisplayTemperature( min ) ); // pad string with spaces for centering
+          }
+        }
+    }
+    // It responds almost immediately. Let's print out the data
+    printTemperature(tempDeviceAddress); // Use a simple function to print out the data
+   } 
+    //else ghost device! Check your power requirements and cabling
+  }
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
