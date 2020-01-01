e10fe65127698c7d7fc6708287a84c8aab49db30
Iza2 <Iza2@Iza2-HP.fios-router.home>
2020-01-01 13:15:06 -0500

    Manual GIT Merger was required.
    Bluetooth: data sent as JSON object instead of just temperature value
    
    Relay Logic updated to have 1minute intervals to avoid flip flop when on
    temperature margin

--- TempController/Relay.ino
+++ TempController/Relay.ino
@@ -1,109 +1,66 @@
-<<<<<<< HEAD
 
+unsigned long relayTimer;
+
+void relaySetup()
+{
+  relayTimer = millis();
+}
 
 void relayControl()
 {
   //
   //  Relay Logic
   //
-  float curRepTemp = getDisplayTemperatureNum( tempC );
-  float TargetRepTemp = getDisplayTemperatureNum( targetTemp );
-  if ( curRepTemp + deviation < TargetRepTemp ) {
-    //
-    //  Too Cold
-    //  Turn On Heat, Turn OffCool
-    //
-    digitalWrite(RelayHeat, LOW); //Turn on relay
-    digitalWrite(RelayCool, HIGH); //Turn off relay
-    if ( RelayStatus != RELAY_HEAT ) {
-      Serial.println( "Heat On" );
-      RelayStatus = RELAY_HEAT;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 3, 1 );            // go to the 2nd row
-        lcd.print( "On " ); 
-      }
-    }
-  }
-  else if ( curRepTemp - deviation > TargetRepTemp ) {
-    digitalWrite(RelayHeat, HIGH); //Turn off relay
-    digitalWrite(RelayCool, LOW); //Turn on relay
-    if ( RelayStatus != RELAY_COOL ) {
-      Serial.println( "Cooling On" );
-      RelayStatus = RELAY_COOL;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 13, 1 );            // go to the 2nd row
-        lcd.print( "On " ); 
-      }
-    }
-  }
-  else {
-    digitalWrite(RelayHeat, HIGH); //Turn off relay
-    digitalWrite(RelayCool, HIGH); //Turn off relay
-    if ( RelayStatus != RELAY_NONE ) {
-      Serial.println( "Heat and cooling off" );
-      RelayStatus = RELAY_NONE;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 3, 1 );            // go to the 2nd row
-        lcd.print( "Off" ); 
-        lcd.setCursor ( 13, 1 );            // go to the 2nd row
-        lcd.print( "Off" ); 
-      }
-    }
-  }
-
-}
-=======
-
-
-void relayControl()
-{
+  unsigned long currentTime = millis();
   //
-  //  Relay Logic
+  //  Engage relay change only at 1 minute intevals
+  //  To stop trigger change when temp is on a margin 
   //
-  float curRepTemp = getDisplayTemperatureNum( tempC );
-  float TargetRepTemp = getDisplayTemperatureNum( targetTemp );
-  if ( curRepTemp + deviation < TargetRepTemp ) {
-    //
-    //  Too Cold
-    //  Turn On Heat, Turn OffCool
-    //
-    digitalWrite(RelayHeat, LOW); //Turn on relay
-    digitalWrite(RelayCool, HIGH); //Turn off relay
-    if ( RelayStatus != RELAY_HEAT ) {
-      Serial.println( "Heat On" );
-      RelayStatus = RELAY_HEAT;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 3, 1 );            // go to the 2nd row
-        lcd.print( "On " ); 
+  if( relayTimer + 60000 < currentTime ){  
+    float curRepTemp = getDisplayTemperatureNum( tempC );
+    float TargetRepTemp = getDisplayTemperatureNum( targetTemp );
+    if ( curRepTemp + deviation < TargetRepTemp ) {
+      //
+      //  Too Cold
+      //  Turn On Heat, Turn OffCool
+      //
+      digitalWrite(RelayHeat, LOW); //Turn on relay
+      digitalWrite(RelayCool, HIGH); //Turn off relay
+      if ( RelayStatus != RELAY_HEAT ) {
+        Serial.println( "Heat On" );
+        RelayStatus = RELAY_HEAT;
+        if( currentDisplayMode == DISPLAYMODE_MAIN ){
+          lcd.setCursor ( 3, 1 );            // go to the 2nd row
+          lcd.print( "On " ); 
+        }
       }
     }
-  }
-  else if ( curRepTemp - deviation > TargetRepTemp ) {
-    digitalWrite(RelayHeat, HIGH); //Turn off relay
-    digitalWrite(RelayCool, LOW); //Turn on relay
-    if ( RelayStatus != RELAY_COOL ) {
-      Serial.println( "Cooling On" );
-      RelayStatus = RELAY_COOL;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 13, 1 );            // go to the 2nd row
-        lcd.print( "On " ); 
+    else if ( curRepTemp - deviation > TargetRepTemp ) {
+      digitalWrite(RelayHeat, HIGH); //Turn off relay
+      digitalWrite(RelayCool, LOW); //Turn on relay
+      if ( RelayStatus != RELAY_COOL ) {
+        Serial.println( "Cooling On" );
+        RelayStatus = RELAY_COOL;
+        if( currentDisplayMode == DISPLAYMODE_MAIN ){
+          lcd.setCursor ( 13, 1 );            // go to the 2nd row
+          lcd.print( "On " ); 
+        }
       }
     }
-  }
-  else {
-    digitalWrite(RelayHeat, HIGH); //Turn off relay
-    digitalWrite(RelayCool, HIGH); //Turn off relay
-    if ( RelayStatus != RELAY_NONE ) {
-      Serial.println( "Heat and cooling off" );
-      RelayStatus = RELAY_NONE;
-      if( currentDisplayMode == DISPLAYMODE_MAIN ){
-        lcd.setCursor ( 3, 1 );            // go to the 2nd row
-        lcd.print( "Off" ); 
-        lcd.setCursor ( 13, 1 );            // go to the 2nd row
-        lcd.print( "Off" ); 
+    else {
+      digitalWrite(RelayHeat, HIGH); //Turn off relay
+      digitalWrite(RelayCool, HIGH); //Turn off relay
+      if ( RelayStatus != RELAY_NONE ) {
+        Serial.println( "Heat and cooling off" );
+        RelayStatus = RELAY_NONE;
+        if( currentDisplayMode == DISPLAYMODE_MAIN ){
+          lcd.setCursor ( 3, 1 );            // go to the 2nd row
+          lcd.print( "Off" ); 
+          lcd.setCursor ( 13, 1 );            // go to the 2nd row
+          lcd.print( "Off" ); 
+        }
       }
     }
+    relayTimer = millis();
   }
-
 }
->>>>>>> branch 'master' of https://github.com/jslawinsk/BrewTempController.git
